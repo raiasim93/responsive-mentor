41e1960efc7f00f15e5c32ba27a7bdb16dcfba74
raiasim93 <asimrai89@gmail.com>
2025-02-23 10:54:04 +1100

    spacing fix: add larger padding on screens larger than 2xl

---
+++ src/components/sections/HomeHeroSection.tsx
@@ -0,0 +1,10 @@
+const HomeHeroSection = () => {
+  return (
+    <>
+    
+    </>
+    
+  )
+}
+
+export default HomeHeroSection
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,8 @@ const config: Config = {
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px',
+      '3xl': '1600px' 
+
     },
   },
   plugins: [],
